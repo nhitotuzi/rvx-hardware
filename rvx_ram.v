a43287eb1f7f37d95fbb454a352b50f72ca459a4
nhitotuzi <noahdelimahitotuzi@gmail.com>
2026-09-17 06:34:52 -0400

    Switch to Sky130 SRAM macros for RAM implementation

--- rvx_ram.v
+++ rvx_ram.v
@@ -2,22 +2,16 @@
 // Copyright (c) 2020-2025 RVX Project Contributors
 
 module rvx_ram #(
-
   // Memory size in bytes
   parameter MEMORY_SIZE      = 8192,
-
   // File with program and data
   parameter MEMORY_INIT_FILE = ""
-
   ) (
-
   // Global signals
-
   input   wire          clock,
   input   wire          reset,
 
   // IO interface
-
   input  wire   [31:0]  rw_address,
   output reg    [31:0]  read_data,
   input  wire           read_request,
@@ -26,67 +20,79 @@ module rvx_ram #(
   input  wire   [3:0 ]  write_strobe,
   input  wire           write_request,
   output reg            write_response
-
   );
 
-  wire                        reset_internal;
-  wire [31:0]                 effective_address;
-  wire                        invalid_address;
+  // Endereço e decodificação (8KB = 2048 palavras de 32-bits)
+  // Cada banco de SRAM do sky130 tem 512 palavras, totalizando 4 bancos
+  wire [10:0] word_addr  = rw_address[12:2];
+  wire [1:0]  bank_sel   = word_addr[10:9];
+  wire [8:0]  macro_addr = word_addr[8:0];
 
-  reg                         reset_reg;
-  reg [31:0]                  ram [0:(MEMORY_SIZE/4)-1];
+  // Chip Select Ativo Baixo (CSB)
+  wire csb0 = ~((read_request | write_request) & (bank_sel == 2'b00));
+  wire csb1 = ~((read_request | write_request) & (bank_sel == 2'b01));
+  wire csb2 = ~((read_request | write_request) & (bank_sel == 2'b10));
+  wire csb3 = ~((read_request | write_request) & (bank_sel == 2'b11));
 
-  always @(posedge clock)
-    reset_reg <= reset;
+  // Write Enable Ativo Baixo (WEB)
+  wire web = ~write_request;
+  
+  // Write Mask Ativo Alto (WMASK)
+  wire [3:0] wmask = write_strobe;
 
-  assign reset_internal = reset | reset_reg;
-  assign invalid_address = $unsigned(rw_address) >= $unsigned(MEMORY_SIZE);
+  // Fios de Saida das macros
+  wire [31:0] dout0, dout1, dout2, dout3;
 
-  integer i;
-  initial begin
-    for (i = 0; i < MEMORY_SIZE/4; i = i + 1) ram[i] = 32'h00000000;
-    if (MEMORY_INIT_FILE != "")
-      $readmemh(MEMORY_INIT_FILE,ram);
-  end
+  sky130_sram_2kbyte_1rw1r_32x512_8 sram_bank0 (
+    .clk0(clock), .csb0(csb0), .web0(web), .wmask0(wmask), .addr0(macro_addr), .din0(write_data), .dout0(dout0),
+    .clk1(1'b0),  .csb1(1'b1), .addr1(9'b0), .dout1()
+  );
 
-  assign effective_address =
-    $unsigned(rw_address[31:0] >> 2);
+  sky130_sram_2kbyte_1rw1r_32x512_8 sram_bank1 (
+    .clk0(clock), .csb0(csb1), .web0(web), .wmask0(wmask), .addr0(macro_addr), .din0(write_data), .dout0(dout1),
+    .clk1(1'b0),  .csb1(1'b1), .addr1(9'b0), .dout1()
+  );
 
+  sky130_sram_2kbyte_1rw1r_32x512_8 sram_bank2 (
+    .clk0(clock), .csb0(csb2), .web0(web), .wmask0(wmask), .addr0(macro_addr), .din0(write_data), .dout0(dout2),
+    .clk1(1'b0),  .csb1(1'b1), .addr1(9'b0), .dout1()
+  );
+
+  sky130_sram_2kbyte_1rw1r_32x512_8 sram_bank3 (
+    .clk0(clock), .csb0(csb3), .web0(web), .wmask0(wmask), .addr0(macro_addr), .din0(write_data), .dout0(dout3),
+    .clk1(1'b0),  .csb1(1'b1), .addr1(9'b0), .dout1()
+  );
+
+  // Registro de seleção para multiplexar no ciclo de read (latência de 1 ciclo)
+  reg [1:0] bank_sel_q;
+  reg reset_reg;
   always @(posedge clock) begin
-    if (reset_internal | invalid_address)
-      read_data <= 32'h00000000;
-    else
-      read_data <= ram[effective_address];
+    bank_sel_q <= bank_sel;
+    reset_reg  <= reset;
   end
 
-  always @(posedge clock) begin
-    if(write_request) begin
-      if(write_strobe[0])
-        ram[effective_address][7:0  ] <= write_data[7:0  ];
-      if(write_strobe[1])
-        ram[effective_address][15:8 ] <= write_data[15:8 ];
-      if(write_strobe[2])
-        ram[effective_address][23:16] <= write_data[23:16];
-      if(write_strobe[3])
-        ram[effective_address][31:24] <= write_data[31:24];
-    end
+  always @(*) begin
+    case(bank_sel_q)
+      2'b00: read_data = dout0;
+      2'b01: read_data = dout1;
+      2'b10: read_data = dout2;
+      2'b11: read_data = dout3;
+    endcase
   end
+
+  wire reset_internal = reset | reset_reg;
 
   always @(posedge clock) begin
     if (reset_internal) begin
       read_response  <= 1'b0;
       write_response <= 1'b0;
-    end
-    else begin
+    end else begin
       read_response  <= read_request;
       write_response <= write_request;
     end
   end
 
   // Avoid warnings about intentionally unused pins/wires
-  wire unused_ok =
-    &{1'b0,
-    effective_address[31:11],
-    1'b0};
+  wire unused_ok = &{1'b0, rw_address[31:13], rw_address[1:0], 1'b0};
 
 endmodule
